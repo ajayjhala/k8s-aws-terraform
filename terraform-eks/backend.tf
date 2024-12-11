5836abe9a540bdb543d887cb57ca0d974b4b1b26
ajayjhala <ajay.jhala88@gmail.com>
2024-12-11 11:56:45 +0530

    first commit

---
+++ terraform-eks/backend.tf
@@ -0,0 +1,8 @@
+#terraform {
+#  backend "s3" {
+#    bucket         = "S3 Bucket name"
+#    key            = "Key or path to the state file"
+#    region         = "S3 buket region"
+#    encrypt        = true
+#  }
+#}
